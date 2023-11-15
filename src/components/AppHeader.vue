91c6f48370190e3a3693b2e80a6341b82995b8c8
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-11-15 15:07:03 +0100

    ADD - scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+
+
+export default  {
+  data (){
+      return {
+        
+    }
+  },
+  methods:{
+
+  },
+  created (){
+      
+  }
+}
+</script>
+
+<template>
+    <div>header</div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<script>
+
+
+export default  {
+  data (){
+      return {
+        
+    }
+  },
+  methods:{
+
+  },
+  created (){
+      
+  }
+}
+</script>
+
+<template>
+    <div>main</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
